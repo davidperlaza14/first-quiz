8cba6093a1da9ee25899e23340f017d796a7838a
davidperlaza14 <davidperlaza14@gmail.com>
2023-10-30 11:40:45 -0500

    Correction of question 2. Done.

--- part1/question2.py
+++ part1/question2.py
@@ -15,7 +15,20 @@
 # it. As always, there is a test suite that checks the result. It is in 
 # `question2_test.py.`
 
-swapper = None
+def swapper(t):
+  """
+  Me parecio mejor  crear una funcion que intercambiara los elemtos de la tupla que, una variable. 
+  
+  Keyword arguments:
+    t (tupla): Esta es la tupla de los dos elementos a intercambiarse.
+  Return: return_description
+    Tuble: Devuelve  la tupla nueva con los items intercambiados.
+  """
+  
+  return (t[1], t[0])
 
 def run_swapper(list_of_tuples):
   return list(map(swapper, list_of_tuples))
+
+
+run_swapper()
