5f3494ed325f758443b96e10ca9ee1958e470170
davidperlaza14 <davidperlaza14@gmail.com>
2023-10-30 20:28:16 -0500

    Correction of question 5. Done.

--- part1/question5.py
+++ part1/question5.py
@@ -20,7 +20,11 @@
 
 sql_create_favorite_foods = """
 
-Your SQL here.
+CREATE TABLE favorite_foods (
+  food_id integer,
+  name text,
+  vegetarian integer
+);
 
 """
 
@@ -30,8 +34,12 @@ Your SQL here.
 
 sql_alter_tables_with_favorite_food = """
 
-Your SQL here.
+ALTER TABLE animals
+ADD COLUMN favorite_food_id integer;
 
+ALTER TABLE people
+ADD COLUMN favorite_food_id integer;
+ 
 """
 
 # Part 5.C:
@@ -40,6 +48,8 @@ Your SQL here.
 
 sql_select_all_vegetarian_pets = """
 
-Your SQL here.
-
+SELECT animals.name, favorite_foods.name
+FROM animals
+JOIN favorite_foods ON animals.favorite_food_id = favorite_foods.food_id
+WHERE favorite_foods.vegetarian = 1;
 """
